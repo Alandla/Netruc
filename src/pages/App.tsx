edf882a6b6beb8b7d44b5347b96a0912524755d3
Alan DELANNÉE <E21C407P@univ-nantes.fr>
2022-04-21 16:49:21 +0200

    Video size

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,32 +1,47 @@
 import React from 'react';
 import YouTube, { YouTubeProps } from 'react-youtube';
 
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import '../style/App.css'
 
 import imgFav from '../../img/Favori.png';
 import imgNext from '../../img/next.png';
 
-const opts = {
-  height: '720',
-  width: '1080',
-  playerVars: {
-    autoplay: 1,
-    rel: 0,
-    loop: 1,
-    controls: 0,
-  },
-};
-
 const App = () => {
+
+  const [width, setWidth] = useState();
+  const [height, setHeight] = useState<number | null>();
+  const ref = useRef(null);
+
+  const opts = {
+    height: height,
+    width: width,
+    playerVars: {
+      autoplay: 1,
+      rel: 0,
+      loop: 1,
+      controls: 0,
+    },
+  };
+
+  function setSizeContent() {
+    if(ref.current != null){
+      setWidth((ref.current as any).clientWidth);
+      setHeight((ref.current as any).clientHeight - 30);
+    }
+  }
+
+  useEffect(() => {
+    setSizeContent();
+  })
 
   return (
     <div className='App'>
       <div className='container'>
         <img src={imgNext} className='imgFav'/>
         <img src={imgFav} className='imgFav'/>
-        <div className='content'>
-          <YouTube videoId="2g811Eo7K8U" opts={opts}/>
+        <div className='content' ref={ref}>
+          <YouTube className="video" videoId="f0X1Tl8aHtA" opts={opts}/>
         </div>
       </div>
     </div>
