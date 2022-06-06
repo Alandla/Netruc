fd0cd785f8cebee460ed69c9eb3d2e96179107ca
HERVOUET Arnaud <arnaud.hervouet@laposte.net>
2022-06-06 11:31:36 +0200

    Petite correction

--- src/pages/Trophe.tsx
+++ src/pages/Trophe.tsx
@@ -15,8 +15,9 @@ const Trophe = () => {
                 <span className='textGras'>OBJECTIFS</span>
                 <Link to="/Succes"><span className='childTitreTrophe'>SUCCÈS</span></Link>
             </div>
-                <CompTrophe valeur={4} valeurMax={10} image={1} texte1={"NOMBRE DE VUES"} texte2={"Regarde 10 vidéos dans la journée."} exp={50} border={1}/>
-                <CompTrophe valeur={5} valeurMax={5} image={2} texte1={"NOMBRE DE FAVORIS"} texte2={"Ajoute 5 vidéos dans tes favoris dans la journée."} exp={100} border={2}/>    
+            <CompTrophe valeur={4} valeurMax={10} image={1} texte1={"NOMBRE DE VUES"} texte2={"Regarde 10 vidéos dans la journée."} exp={50} border={1} />
+            <hr />
+            <CompTrophe valeur={5} valeurMax={5} image={2} texte1={"NOMBRE DE FAVORIS"} texte2={"Ajoute 5 vidéos dans tes favoris dans la journée."} exp={100} border={2} />
         </div>
     );
 };
